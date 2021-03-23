1d75cb981765721af3c0793a26e48df9665c3963
roberto <rdgc18@gmail.com>
2021-03-22 22:04:31 -0500

    avance responsive

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,12 @@
 function iniciarMap(){ 
-    var coord = {lat:0.925611 ,lng: -79.659523};
-    var map = new google.maps.Map(document.getElementById('map'),{
+    let coord = {lat:0.925611 ,lng: -79.659523};
+    let map = new google.maps.Map(document.getElementById('map'),{
     zoom: 10,
     center: coord
     });
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
     position: coord,
     map: map
     });
 }
+

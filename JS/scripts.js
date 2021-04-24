04a79970bd713b9f1e6d560ee73b73ca1bfdf29a
roberto <rdgc18@gmail.com>
2021-04-24 11:49:29 -0500

    final

--- JS/scripts.js
+++ JS/scripts.js
@@ -14,22 +14,22 @@ const hamburguer=document.getElementById('hamburguer');
 const menu=document.getElementById('menu');
 
 hamburguer.addEventListener('click',()=>{
-    menu.classList.toggle('show');
-    if(menu.classList.contains('show'))
-        hamburguer.firstChild.textContent='X';
-    else
-        hamburguer.firstChild.innerHTML='&#9776;';
+    validarOpenClose(menu);
 })
 
 window.onload = function() {
     let menu_item = document.querySelectorAll('.menu-item');
     for (const item of menu_item) {
     item.addEventListener('click',()=>{
-        menu.classList.toggle('show');
+        validarOpenClose(menu);
+    })
+    }
+}
+
+const validarOpenClose=(element)=>{
+    element.classList.toggle('show');
         if(menu.classList.contains('show'))
             hamburguer.firstChild.textContent='X';
         else
             hamburguer.firstChild.innerHTML='&#9776;';
-    })
-    }
 }
